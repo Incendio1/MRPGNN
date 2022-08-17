17f12fe4f89ab83f41e0d212ae31a8a0caf629e4
Incendio1 <96165724+Incendio1@users.noreply.github.com>
2022-08-17 15:45:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ GPU=0
 
 echo "=====Cora====="
 echo "---Fiexd Split---"
-CUDA_VISIBLE_DEVICES=${GPU} python crdgnn.py --dataset=Cora --K=10 --dropout=0.85 --dropnode_rate=0.15
+CUDA_VISIBLE_DEVICES=${GPU} python trdgnn.py --dataset=Cora --K=10 --dropout=0.85 --dropnode_rate=0.15
 
 echo "=====Cornell====="
 echo "---Dense Split---"
-CUDA_VISIBLE_DEVICES=${GPU} python crdgnn.py --dataset=cornell --dropout=0.5 --alpha=0.35 --beta=0.2 --dropnode_rate=0.15
+CUDA_VISIBLE_DEVICES=${GPU} python trdgnn.py --dataset=cornell --dropout=0.5 --alpha=0.35 --beta=0.2 --dropnode_rate=0.15
 
 
 
